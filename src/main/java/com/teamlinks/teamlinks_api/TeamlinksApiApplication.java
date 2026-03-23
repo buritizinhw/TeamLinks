d23af6b580e37aa3283f482b35d11b971a3d8a68
Lívia Carvalho Pereira Buriti <liviacburiti@outlook.com>
2026-03-23 14:46:19 -0300

    first commit

---
+++ src/main/java/com/teamlinks/teamlinks_api/TeamlinksApiApplication.java
@@ -0,0 +1,13 @@
+package com.teamlinks.teamlinks_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamlinksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamlinksApiApplication.class, args);
+	}
+
+}
