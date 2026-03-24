99dec59500d8fd8fda6f4ccf218464e4db3b47c0
Lívia Carvalho Pereira Buriti <liviacburiti@outlook.com>
2026-03-24 19:11:28 -0300

    feat:create project entity

---
+++ src/main/java/com/teamlinks/teamlinks_api/entity/Link.java
@@ -0,0 +1,54 @@
+package com.teamlinks.teamlinks_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.Set;
+
+@Entity
+@Table(name = "links")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String name;
+
+    @CreationTimestamp
+    @UpdateTimestamp
+    @Column(nullable = true)
+    private Instant UpdatedAt;
+
+    @Column(nullable = false)
+    private Long clicks=0L;
+
+    @ManyToMany
+    @JoinTable(
+            name = "link_tags",
+            joinColumns = @JoinColumn(name = "link_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+
+
+}
---
+++ src/main/java/com/teamlinks/teamlinks_api/entity/Project.java
@@ -0,0 +1,34 @@
+package com.teamlinks.teamlinks_api.entity;
+
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "projects")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+
+
+}
